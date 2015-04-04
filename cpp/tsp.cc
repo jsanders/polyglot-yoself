6653c32b2d70d27f42f0cc1ab43c753216fc9b76
James Sanders <sanderjd@gmail.com>
2015-04-03 23:24:44 -0600

    Working nearest neighbor heuristic

--- cpp/tsp.cc
+++ cpp/tsp.cc
@@ -1,44 +1,94 @@
 #include <iostream>
 #include <string>
 #include <set>
+#include <vector>
+#include <cmath>
 
+using ::std::ostream;
 using ::std::cout;
+using ::std::endl;
 using ::std::string;
 using ::std::set;
+using ::std::vector;
+using ::std::abs;
+using ::std::sqrt;
 
 struct Point {
   int x;
   int y;
-
-  // A bit unfortunate, but I want to put `Point`s in `set`s, and they are ordered.
-  bool operator<(const Point other) const {
-    // Use lexicographic order, which I believe to satisfy the strict weak ordering requirement.
-    return (x != other.x && x < other.x) || y < other.y;
-  }
 };
 
-void printPoint(Point point) {
-  cout << "(" << point.x << ", " << point.y << ")\n";
+// Some useful operations for Points
+
+ostream& operator<< (ostream& os, const Point& p) {
+  os << "(" << p.x << ", " << p.y << ")";
+  return os;
 }
 
-void print(set<Point> points, string title) {
-  cout << "--- " << title << " ---\n";
+bool operator<(const Point a, const Point b) {
+  // Use lexicographic order, which I believe to satisfy the strict weak ordering requirement.
+  return (a.x != b.x && a.x < b.x) || a.y < b.y;
+}
+
+double distance(const Point a, const Point b) {
+  auto xdist = abs(a.x - b.x);
+  auto ydist = abs(a.y - b.y);
+  return sqrt(xdist * xdist + ydist * ydist);  
+}
+
+// Printing stuff
+
+template <typename T>
+void banner(T v) {
+  cout << "----- " << v << " -----" << endl;
+}
+
+template <typename T>
+void print(T points) {
   for (const auto &p : points) {
-    printPoint(p);
+    cout << p << endl;
   }
+}
+
+// Find nearest neighbor in given set
+Point nearest_neighbor(Point point, set<Point> neighbors) {
+  auto it = neighbors.begin();
+  Point nearest = *it;
+  double dist = distance(point, nearest);
+  for (; it != neighbors.end(); ++it) {
+    double _dist = distance(point, *it);
+    if (_dist < dist) {
+      nearest = *it;
+      dist = _dist;
+    }
+  }
+  return nearest;
+}
+
+// Find a path using the nearest neighbor heuristic
+vector<Point> nearest_neighbor_heuristic(set<Point> points) {
+  vector<Point> visited;
+  Point last;
+  Point current = *points.begin();
+
+  while(true) {
+    visited.push_back(current);
+    points.erase(current);
+    last = current;
+
+    if (points.size() == 0) {
+      break;
+    } else {
+      current = nearest_neighbor(current, points);
+    }
+  }
+
+  return visited;
 }
 
 int main() {
-  set<Point> unvisited = { {1,1},{2,3},{3,1},{5,7} };
-  set<Point> visited;
-  for (auto p = unvisited.begin(); p != unvisited.end(); ) {
-    cout << "--- current ---\n";
-    auto cur = *p;
-    printPoint(cur);
-    visited.insert(cur);
-    p = unvisited.erase(p);
-    print(visited, "visited");
-    print(unvisited, "unvisited");
-  }
-  print(visited, "final");
+  set<Point> points = { {1,1},{2,3},{3,1},{5,7} };
+  auto solution = nearest_neighbor_heuristic(points);
+
+  print(solution);
 }
