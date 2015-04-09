1a2ced3b6e3b3f3d8f06cd97e21ec4f2cdd7f250
James Sanders <sanderjd@gmail.com>
2015-04-09 08:48:43 -0600

    Working versin of population by year in rust
    Basically a direct port of the C++98 version. Lots of mutability. Would
    be fun to try to improve.

--- rust/population_by_year.rs
+++ rust/population_by_year.rs
@@ -1,28 +1,38 @@
-static FIRST_YEAR: i32 = 1900;
-static LAST_YEAR: i32 = 2000;
-// static NUM_YEARS: usize = (LAST_YEAR - FIRST_YEAR + 1);
+// Pretty much a direct port of the C++98 version. Probably lots of room for improvement!
+
+static FIRST_YEAR: usize = 1900;
+static LAST_YEAR: usize = 2000;
 
 struct Person {
-  birth_year: i32,
-  death_year: i32
+  birth_year: usize,
+  death_year: usize
 }
 
-fn find_max(people: Vec<Person>) -> (i32, i32) {
-  let years = [0i32; 101];
+impl Person {
+  fn new(birth_year: usize, death_year: usize) -> Person {
+    let error = format!("Must be born between {} and {}", FIRST_YEAR, LAST_YEAR);
+    assert!(birth_year >= FIRST_YEAR, error);
+    assert!(death_year <= LAST_YEAR, error);
+    Person { birth_year: birth_year, death_year: death_year }
+  }
+}
+
+fn find_max(people: Vec<Person>) -> (usize, i32) {
+  let mut years = [0i32; 101];
 
   for person in people.iter() {
     let birth_year_idx = person.birth_year - FIRST_YEAR;
     let death_year_idx = person.death_year - FIRST_YEAR;
-    years[birth_year_idx] += 1;
-    years[death_year_idx] -= 1;
+    years[birth_year_idx] += 1i32;
+    years[death_year_idx] -= 1i32;
   }
 
   let (mut current, mut max) = (0i32, 0i32);
-  let max_year = FIRST_YEAR;
-  for &year in years.iter() {
-    current += year;
+  let mut max_year = FIRST_YEAR;
+  for (idx, year) in years.iter().enumerate() {
+    current += *year;
     if current > max {
-      max_year = FIRST_YEAR;
+      max_year = FIRST_YEAR + idx;
       max = current;
     }
   }
