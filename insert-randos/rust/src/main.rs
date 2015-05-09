f2137eb79f6ebfd36cc03a6efdb74d74d3b7a218
James Sanders <sanderjd@gmail.com>
2015-05-09 01:38:09 -0600

    Genericize

--- insert-randos/rust/src/main.rs
+++ insert-randos/rust/src/main.rs
@@ -6,6 +6,6 @@ use sorted_vec::SortedVec;
 
 fn main() {
     let mut rng = rand::thread_rng();
-    let vec: SortedVec = rng.gen_iter::<u8>().take(100).collect();
+    let vec: SortedVec<_> = rng.gen_iter::<i64>().take(100).collect();
     println!("{:?}", vec);
 }
--- insert-randos/rust/src/sorted_vec.rs
+++ insert-randos/rust/src/sorted_vec.rs
@@ -1,12 +1,12 @@
 use std::{fmt,iter};
 
-pub struct SortedVec {
-    raw: Vec<u8>
+pub struct SortedVec<T> {
+    raw: Vec<T>
 }
 
-impl SortedVec {
-    fn push(&mut self, val: u8) {
-        let index = self.raw.iter().position(|&elem| val < elem);
+impl<T: PartialOrd> SortedVec<T> {
+    fn push(&mut self, val: T) {
+        let index = self.raw.iter().position(|elem| val < *elem);
 
         match index {
             Some(_index) => self.raw.insert(_index, val),
@@ -15,14 +15,14 @@ impl SortedVec {
     }
 }
 
-impl fmt::Debug for SortedVec {
+impl<T: fmt::Debug> fmt::Debug for SortedVec<T> {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         write!(f, "{:?}", self.raw)
     }
 }
 
-impl iter::FromIterator<u8> for SortedVec {
-    fn from_iter<I: IntoIterator<Item=u8>>(iterable: I) -> SortedVec {
+impl<T: PartialOrd> iter::FromIterator<T> for SortedVec<T> {
+    fn from_iter<I: IntoIterator<Item=T>>(iterable: I) -> SortedVec<T> {
         let iterator = iterable.into_iter();
         let (lower, _) = iterator.size_hint();
         let mut vec = SortedVec { raw: Vec::with_capacity(lower) };
